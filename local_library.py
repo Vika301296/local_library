36172bef3bc48f49584a1920eb426f00d546b457
Виктория Федорова <fiodorova.vika301296@yandex.ru>
2023-09-28 01:22:20 +0300

    current_varient

--- local_library.py
+++ local_library.py
@@ -7,34 +7,38 @@ from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from urllib.parse import urljoin, urlsplit
 
-warnings.filterwarnings("ignore")
-# Disable SSL certificate verification warnings
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-urllib3.disable_warnings(urllib3.exceptions.NotOpenSSLWarning)
-folder = "books"
-current_directory = os.getcwd()
-images_directory = os.path.join(current_directory, 'images')
-
-# Create the 'images' directory if it doesn't exist
-os.makedirs(images_directory, exist_ok=True)
-# /Users/viktoriafedorova/Desktop/Devman/local_library/images
 url = "https://tululu.org/b9/"
 
 
-def get_comments(url):
-    response = requests.get(url, verify=False, allow_redirects=False)
-    soup = BeautifulSoup(response.text, "lxml")
-    comments = soup.find_all("div", class_="texts")
-    comment_texts = []
-    for comment in comments:
-        full_comment = comment.get_text()
-        full_comment.split(')')
-        comment_text = (full_comment.split(')'))[1].strip()
-        comment_texts.append(comment_text)
-    return comment_texts
+def check_for_redirect(response):
+    if response.status_code != 200:
+        raise requests.HTTPError
 
 
-# print(get_comments(url))
+def parse_book_page(url):
+    try:
+        response = requests.get(url, verify=False, allow_redirects=False)
+        soup = BeautifulSoup(response.text, "lxml")
+        genres = (soup.find(
+            "span", class_="d_book").find('a')['title']).split('-')[0]
+        comments = soup.find_all("div", class_="texts")
+        comment_texts = []
+        for comment in comments:
+            full_comment = comment.get_text()
+            full_comment.split(')')
+            comment_text = (full_comment.split(')'))[1].strip()
+            comment_texts.append(comment_text)
+        book_title_and_author = soup.find("h1").text.split("::")
+        book_title, book_author = (book_title_and_author[0].strip(),
+                                   book_title_and_author[1].strip())
+        book_info = {'title': f'{book_title}',
+                     'author': f'{book_author}',
+                     'genres': f'{genres}',
+                     'comments': f'{comment_texts}'}
+        return book_info
+    except requests.HTTPError:
+        print(f"HTTPError for book {url}")
+        return None  # Return None if there's an error
 
 
 def get_bookimage(url):
@@ -62,46 +66,46 @@ def download_bookimage(url, folder='images'):
         file.write(response.content)
 
 
-def get_book_title(url):
-    response = requests.get(url, verify=False, allow_redirects=False)
-    try:
-        check_for_redirect(response)
-        soup = BeautifulSoup(response.text, "lxml")
-        book_title_and_author = soup.find("h1").text.split("::")
-        book_title = book_title_and_author[0].strip()
-        book_author = book_title_and_author[1].strip()
-        return book_title
-    except requests.HTTPError:
-        print(f"HTTPError for book {url}")
-
-
 def download_txt(url, filename, folder="books/"):
     filename = sanitize_filename(filename)
     filepath = os.path.join(folder, f"{filename}.txt")
     return filepath
 
 
-def check_for_redirect(response):
-    if response.status_code != 200:
-        raise requests.HTTPError
-
-
 for book in range(1, 11):
     download_url = f'https://tululu.org/txt.php?id={book}'
     info_url = f'https://tululu.org/b{book}/'
-    book_title = get_book_title(info_url)
-    books_filepath = download_txt(download_url, book_title)
-    response = requests.get(download_url, verify=False, allow_redirects=False)
-    book_image_url = get_bookimage(info_url)
-    print(get_comments(info_url))
-    if book_image_url == "No picture available":
-        print(f"The book {book_title} has no picture.")
-    else:
-        download_bookimage(book_image_url)
+    try:
+        book_info = parse_book_page(info_url)
+
+        if book_info is not None:
+            # Continue with the rest of your code here
+            pass
+        else:
+            print(f"Skipping book {book} due to error.")
+
+    except requests.HTTPError:
+        print(f"HTTPError for book {book}")
+        continue  # Continue to the next book
+    except Exception as e:
+        print(f"Error for book {book}: {e}")
+        continue  # Continue to the next book
+
+    # book_title = get_book_title(info_url)
+    # books_filepath = download_txt(download_url, book_title)
+    # response = requests.get(download_url, verify=False, allow_redirects=False)
+    # book_image_url = get_bookimage(info_url)
+    # if book_image_url == "No picture available":
+    #     print(f"The book {book_title} has no picture.")
+    # else:
+    #     download_bookimage(book_image_url)
 
     # try:
-    #     check_for_redirect(response)
-    #     with open(books_filepath, 'wb') as file:
-    #         file.write(response.content)
-    # except requests.HTTPError:
-    #     print(f"HTTPError for book {book}")
+    #     genres = get_genres(info_url)
+    #     if genres is not None:
+    #         # Continue with the rest of your code here
+    #         pass
+    #     else:
+    #         print(f"Skipping book {book} due to error.")
+    # except Exception as e:
+    #     print(f"Error for book {book}: {e}")
