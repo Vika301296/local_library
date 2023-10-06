524ca169379569571d9970ce5234ecc6ac6f8f06
Виктория Федорова <fiodorova.vika301296@yandex.ru>
2023-10-06 10:38:19 +0300

    Fix the errors

--- parse_tululu.py
+++ parse_tululu.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import requests
+import time
 import urllib3
 
 from bs4 import BeautifulSoup
@@ -14,41 +15,37 @@ def check_for_redirect(response):
         raise requests.HTTPError('Redirect detected')
 
 
-def parse_book_page(url):
-    response = requests.get(url, verify=False, allow_redirects=False)
+def parse_book_page(response):
     check_for_redirect(response)
-    try:
-        soup = BeautifulSoup(response.text, "lxml")
-        genres = (soup.find(
-            "span", class_="d_book").find('a')['title']).split('-')[0]
-        comments = soup.find_all("div", class_="texts")
-        comment_texts = []
-        for comment in comments:
-            full_comment = comment.get_text()
-            full_comment.split(')')
-            comment_text = (full_comment.split(')'))[1].strip()
-            comment_texts.append(comment_text)
-        book_title_and_author = soup.find("h1").text.split("::")
-        book_title, book_author = (book_title_and_author[0].strip(),
-                                   book_title_and_author[1].strip())
-        bookimage_div = soup.find("div", class_="bookimage")
-        if bookimage_div:
-            book_image = bookimage_div.find("img")['src']
-            book_image_url = urljoin('https://tululu.org/', book_image)
-        book_info = {'title': f'{book_title}',
-                     'author': f'{book_author}',
-                     'genres': f'{genres}',
-                     'comments': f'{comment_texts}',
-                     'book_image_url': f'{book_image_url}'}
-        return book_info
-    except requests.HTTPError:
-        print(f"HTTPError for book {url}")
+    soup = BeautifulSoup(response.text, "lxml")
+    all_genres = [genre.text for genre in soup.find(
+        'span', class_='d_book').find_all('a')]
+    comments = soup.find_all("div", class_="texts")
+    comment_texts = []
+    for comment in comments:
+        full_comment = comment.get_text()
+        full_comment.split(')')
+        comment_text = (full_comment.split(')'))[1].strip()
+        comment_texts.append(comment_text)
+    book_title_and_author = soup.find("h1").text.split("::")
+    book_title, book_author = (book_title_and_author[0].strip(),
+                               book_title_and_author[1].strip())
+    bookimage_div = soup.find("div", class_="bookimage")
+    if bookimage_div:
+        book_image = bookimage_div.find("img")['src']
+        book_image_url = urljoin(response.url, book_image)
+    parsed_book_page = {'title': f'{book_title}',
+                        'author': f'{book_author}',
+                        'genres': all_genres,
+                        'comments': comment_texts,
+                        'book_image_url': f'{book_image_url}'}
+    return parsed_book_page
 
 
 def download_bookimage(url, folder='images'):
     image_name = os.path.basename(urlsplit(
         url, scheme='', allow_fragments=True)[2])
-    response = requests.get(url, verify=False, allow_redirects=False)
+    response = requests.get(url, verify=False)
     response.raise_for_status()
     check_for_redirect(response)
     filename = os.path.join(
@@ -60,7 +57,7 @@ def download_bookimage(url, folder='images'):
 def download_book(url, payload, filename, folder="books/"):
     filepath = os.path.join(folder, f"{filename}.txt")
     response = requests.get(
-        url, params=payload, verify=False, allow_redirects=False)
+        url, params=payload, verify=False)
     response.raise_for_status()
     check_for_redirect(response)
     with open(filepath, 'wb') as file:
@@ -80,29 +77,24 @@ if __name__ == "__main__":
     for book in range(args.start_id, args.end_id + 1):
         download_url = 'https://tululu.org/txt.php'
         download_payload = {'id': book}
-        book_page = f'https://tululu.org/b{book}/'
+        book_page_url = f'https://tululu.org/b{book}/'
         try:
             response = requests.get(
-                book_page, verify=False, allow_redirects=False)
+                book_page_url, verify=False)
             response.raise_for_status()
             check_for_redirect(response)
-            soup = BeautifulSoup(response.text, "lxml")
-            book_title = parse_book_page(book_page)['title']
+            parsed_book_page = parse_book_page(response)
+            book_title = parsed_book_page['title']
             download_book(download_url, download_payload, book_title)
-            download_bookimage(parse_book_page(book_page)['book_image_url'])
-            comments = parse_book_page(book_page)['comments']
+            image_url = parsed_book_page['book_image_url']
+            download_bookimage(image_url)
+            comments = parsed_book_page['comments']
             filename = os.path.join('comments', f'{book_title}.txt')
             with open(filename, 'w', encoding='utf-8') as file:
                 for comment in comments:
-                    full_comment = comment.get_text()
-                    full_comment.split(')')
-                    comment_text = (full_comment.split(')'))[1].strip()
-                    file.write(comment_text + '\n')
+                    file.write(comment + '\n')
         except requests.ConnectionError:
             print('Connection Error')
-            pass
+            time.sleep(15)
         except requests.HTTPError as e:
             print(f'HTTPError: {e}')
-            pass
-        except AttributeError:
-            pass
